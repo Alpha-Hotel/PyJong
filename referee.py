b703126e0035a1054f647f9e1c982454bbcc4194
Alpha-Hotel <83920550+Alpha-Hotel@users.noreply.github.com>
2023-11-02 19:22:04 -0600

    Update referee.py
    More tests
    documentation
    fixed picking up tiles

--- referee.py
+++ referee.py
@@ -110,28 +110,19 @@ class Referee:
             return None, None
 
     def check_chi(self, players, current_discard, test):
-
-           
-            new_list = []
-            #lmao im so funny
-            cheese_list_1 = []
-            cheese_list_2 = []
-            cheese_list_3 = []
-            adjacency_check = False
-            list_length = int(len(new_list))
-
-            if current_discard.name[4].isdigit():
-                for i in range(len(players)):
-                    new_list = [tile for tile in players[i].hand if current_discard.name in tile.edge]
+            '''Check if it is possible for any of the players to call chi on the current discard.'''
+            if current_discard.name[4].isdigit(): #Non-honor
+                for i in range(len(players)): 
+                    new_list = [tile for tile in players[i].hand if current_discard.name in tile.edge] #tiles adjacent to current discard
                     for node in new_list:
                         new_list = new_list + [tile for tile in players[i].hand if node.name in tile.edge and not tile.name == current_discard.name ]
+                        #tiles adjacent to tiles adjacent to current discard
 
                     if len(new_list) >= 2 and len(set(Counter(new_list).keys())) >= 2:
-                        
                         if i == 3 or test: #player decisions
                             player = self.player_chi_choice(players[i], new_list, current_discard)
                             if not player == None:
-                                return player, i
+                                return player, i #TODO add headbump
                             else:
 
                                 return None, None
@@ -149,42 +140,35 @@ class Referee:
                                 print("AI player didn't call chi on: ", current_discard, ". What a rookie.")
                                 return None, None
 
-                           
-                    pass
-                pass
-    
+                               
     def player_chi_choice(self, player, new_list, current_discard):
-        pc_decision_chi = input("Do you want to call chi? y/n   ")
-
-        if pc_decision_chi == "y":
-
-            if len(set(Counter(new_list).keys())) >= 3:
-
-                
-                two_to_left = [tile for tile in new_list if int(tile.name[-1]) == int(current_discard.name[-1])-2]
-                one_to_left = [tile for tile in new_list if int(tile.name[-1]) == int(current_discard.name[-1])-1]
-                two_to_right = [tile for tile in new_list if int(tile.name[-1]) == int(current_discard.name[-1])+2]
-                one_to_right = [tile for tile in new_list if int(tile.name[-1]) == int(current_discard.name[-1])+1]
-                chi_choices = []
-                if two_to_left and one_to_left:
-                    chi_choices.append([two_to_left[0], one_to_left[0], current_discard])
-                if one_to_left and one_to_right:
-                    chi_choices.append([ one_to_left[0], current_discard, one_to_right[0]])
-                if one_to_right and two_to_right:
-                    chi_choices.append([ current_discard, one_to_right[0], two_to_right[0]])
-                
-                print(self.str_chi_options(chi_choices))
-            current_discard_list = [current_discard]
+        '''Function for checking if the player wants to call chi on one of all possible combinations.'''
+        two_to_left = [tile for tile in new_list if int(tile.name[-1]) == int(current_discard.name[-1])-2]
+        one_to_left = [tile for tile in new_list if int(tile.name[-1]) == int(current_discard.name[-1])-1]
+        two_to_right = [tile for tile in new_list if int(tile.name[-1]) == int(current_discard.name[-1])+2]
+        one_to_right = [tile for tile in new_list if int(tile.name[-1]) == int(current_discard.name[-1])+1]
+        chi_choices = []
+        if two_to_left and one_to_left:
+            chi_choices.append([two_to_left[0], one_to_left[0], current_discard])
+        if one_to_left and one_to_right:
+            chi_choices.append([ one_to_left[0], current_discard, one_to_right[0]])
+        if one_to_right and two_to_right:
+            chi_choices.append([ current_discard, one_to_right[0], two_to_right[0]])
+        pc_decision_chi = input(self.str_chi_options(chi_choices))
+        if not pc_decision_chi == "n":
             player.closed_hand = False
             player.hand=player.hand + [current_discard]
-            return player #TODO add headbump
+            #TODO add lock here
+        return player 
 
     def str_chi_options(self, chi_choices):
+        '''Creates "pretty" string for asking a user whether they want to call chi'''
         string = "\nChoose from the following Chi options:\n"
         for choice in range(len(chi_choices)):
             string += f"{choice}: {chi_choices[choice][0]}, {chi_choices[choice][1]}, {chi_choices[choice][2]}\n"
         string += f"n: Don't call Chi\n"
         return string
+
     def check_exhaustive_draw(self):
         pass
 
@@ -203,20 +187,30 @@ class Referee:
 
 
 class Referee_Test_Suite:
-    def __init__(self):
+    def __init__(self, ref):
         self.player = Player()
-
+        self.run_tests(ref)
+        
+    def run_tests(self, ref):
+        '''Run tests is set up to run the all of the tests that have been written. It will
+        '''
+        print(self.str_test([self.test_chi_1(ref.check_chi),
+        self.test_chi_2(ref.check_chi)]))
     def test_chi_1(self, check_chi):
         self.player.hand = [Sou_2(),Sou_3()]
-        check_chi([self.player], Sou_4(), True)
+        player, _ = check_chi([self.player], Sou_4(), True)
+        x = [tile.name for tile in player.hand]
+        assert x == ['Sou_2', 'Sou_3', 'Sou_4'] or ['Sou_2', 'Sou_3']
+        return True
     def test_chi_2(self, check_chi):
         self.player.hand = [Sou_4(),Sou_5(),Sou_5(),Sou_6(), Sou_7()]
-        check_chi([self.player], Sou_6(), True)
-        
+        player, _ = check_chi([self.player], Sou_6(), True)
+        x = [tile.name for tile in player.hand]
+        assert x == ['Sou_4', 'Sou_5', 'Sou_5', 'Sou_6', 'Sou_7', 'Sou_6'] or ['Sou_4', 'Sou_5', 'Sou_5', 'Sou_6', 'Sou_7']
+        return True
+    def str_test(self, li):
+        return f"{sum(li)} out of {len(li)} tests passed."
 
 if __name__ == "__main__":
-
-    ref = Referee()
-    test = Referee_Test_Suite()
-    test.test_chi_1(ref.check_chi)
-    test.test_chi_2(ref.check_chi)
+    test = Referee_Test_Suite(Referee())
+    
